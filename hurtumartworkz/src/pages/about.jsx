3cb73efae047a8807676e430274800e6e8fe7580
DanaeD1867 <116097166+DanaeD1867@users.noreply.github.com>
2024-06-05 19:08:51 -0700

    create skeleton for about & services pages

--- hurtumartworkz/src/pages/about.jsx
+++ hurtumartworkz/src/pages/about.jsx
@@ -1,7 +1,21 @@
 import React from "react"; 
 
 function About(){
-  return(<div>About</div>);
+  return(<div>
+    <div>
+      <h1>Our Story</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, atque! Earum, odio? Quis nulla impedit corrupti. Repellat ad cumque magni eum at eius, molestiae, minima quaerat eveniet fugiat voluptas, deleniti delectus inventore consequatur omnis reiciendis hic dolor atque nam quod repudiandae. Commodi vel quod consequatur rem necessitatibus voluptatibus provident dolore?</p>
+      <button>Support Us</button>
+    </div>
+
+    <div>
+      <h1>Reviews</h1>
+    </div>
+
+
+  </div>
+
+  );
 };
 
 export default About;
--- hurtumartworkz/src/pages/services.jsx
+++ hurtumartworkz/src/pages/services.jsx
@@ -1,7 +1,39 @@
 import React from "react"; 
 
 function Services(){
-  return(<div>Services</div>);
+  return(<div>
+    
+    <title>Services</title>
+
+    <div>
+      <div>
+        <h3>Paint Parties</h3>
+        <p>Describe</p>
+        <button>Shop Now</button>
+      </div>
+
+      <div>
+        <h3>Custom Tees</h3>
+        <p>Describe</p>
+        <button>Shop Now</button>
+      </div>
+
+      <div>
+        <h3>Original Pieces</h3>
+        <p>Describe</p>
+        <button>Shop Now</button>
+      </div>
+
+      <div>
+        <h3>Murals</h3>
+        <p>Describe</p>
+        <button>Shop Now</button>
+      </div>
+    </div>
+
+  </div>
+  
+);
 };
 
 export default Services; 
