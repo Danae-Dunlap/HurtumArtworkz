fa71b32d67a67a3e0f0817635cc5cc9d250064ab
DanaeD1867 <116097166+DanaeD1867@users.noreply.github.com>
2024-07-08 21:17:02 -0700

    edited index file

--- hurtumartworkz/src/pages/index.jsx
+++ hurtumartworkz/src/pages/index.jsx
@@ -1,13 +1,14 @@
 import React from "react"; 
+import "./index.css"; 
 
 function Home (){
   return(
     <div>
-      <div>
+      <div className="openCarousel">
         <h1> Check Out Our Amazing Works</h1>
         <button>Shop Now</button>
       </div>
-      <div>
+      <div className="introSection">
         <h2>The Most Authentic Pieces For the Most Authentic Prices</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae fugiat voluptate omnis veniam animi reprehenderit sequi quod officia cumque porro quia tempora laudantium facilis nobis quis quibusdam ipsum voluptas exercitationem illum debitis delectus blanditiis vero, error accusantium. Nemo iusto enim labore quaerat sequi neque quod laudantium, quia quis, error iure.</p>
         <button>Learn More</button>
